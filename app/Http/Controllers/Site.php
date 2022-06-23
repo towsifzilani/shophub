e41244cca8dc6d642aa58e86298360384e31688a
zilanise <zilanise@gmail.com>
2022-06-24 00:44:01 +0600

    Login preferences

--- app/Http/Controllers/Site.php
+++ app/Http/Controllers/Site.php
@@ -4,11 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\File;
 
-class Site extends Controller
+class Site extends Home
 {
     public function __construct()
     {
@@ -18,37 +19,35 @@ class Site extends Controller
     public function index()
     {
         $data['view'] = 'site.home';
-        return $this->loadview($data);
-    }
-
-    public function loadview($data=[])
-    {
-        if(!isset($data['view'])) $data['view'] = 'site.home';
-        return view($data['view']);
-
+        return $this->viewLoader($data);
     }
 
     public function blog_list()
     {
         $data['view'] = 'site.blogs';
-        return $this->loadview($data);
+        return $this->viewLoader($data);
     }
 
     public function site_contact()
     {
         $data['view'] = 'site.contact';
-        return $this->loadview($data);
+        return $this->viewLoader($data);
     }
 
     public function load_shop_page()
     {
         $data['view'] = 'site.shop';
-        return $this->loadview($data);
+        return $this->viewLoader($data);
     }
 
     public function login_page()
     {
         $data['view'] = 'site.login';
-        return $this->loadview($data);
+        return $this->viewLoader($data);
+    }
+
+    public function user_engage(Request $request)
+    {
+        
     }
 }
