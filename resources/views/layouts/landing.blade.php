1176ae0cfb65c5b36a84179372fccde4915aeb4c
zilanise <zilanise@gmail.com>
2022-06-24 23:46:48 +0600

    Login preferences

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="author" content="">
     <title>@yield('title') | {{ config('app.name') }} </title>
     <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet">
--- resources/views/site/login.blade.php
+++ resources/views/site/login.blade.php
@@ -7,30 +7,37 @@
 			<div class="row">
 				<div class="col-sm-4 col-sm-offset-1">
 					<div class="login-form"><!--login form-->
-						<h2>Login to your account</h2>
-						<form action="#">
+						<h2>{{ __('Login to your account') }}</h2>
+						<form action="{{ route('site-user-engage') }}" method="POST">
+							@csrf
+							<input type="hidden" 
+								name="engageType" 
+								id="engageType" 
+								value="login">
 							<input type="email" placeholder="Enter Email"/>
 							<input type="password" placeholder="Enter Password"/>
 							<span>
 								<input type="checkbox" class="checkbox"> 
 								Keep me signed in
 							</span>
-							<button type="submit" class="btn btn-default">Login</button>
+							<button type="submit" class="btn btn-default">{{ __('Login') }}</button>
 						</form>
 					</div><!--/login form-->
 				</div>
 				<div class="col-sm-1">
-					<h2 class="or">OR</h2>
+					<h2 class="or">{{ __('OR') }}</h2>
 				</div>
 				<div class="col-sm-4">
 					<div class="signup-form"><!--sign up form-->
-						<h2>New User Signup!</h2>
-						<form action="#">
+						<h2>{{ __('New User Signup!') }}</h2>
+						<form action="{{ route('site-user-engage') }}" method="POST">
+							@csrf
+							<input type="hidden" name="engageType" id="engageType" value="signup">
 							<input type="text" placeholder="Name"/>
 							<input type="email" placeholder="Email Address"/>
 							<input type="password" placeholder="Password"/>
 							<input type="password" placeholder="confirm Password"/>
-							<button type="submit" class="btn btn-default">Signup</button>
+							<button type="submit" class="btn btn-default">{{ __('Signup') }}</button>
 						</form>
 					</div><!--/sign up form-->
 				</div>
